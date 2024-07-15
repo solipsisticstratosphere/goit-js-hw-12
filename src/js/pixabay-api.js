5d1c9226f5f34ace5164ba4dcc0242e0b3b06f94
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-07-15 20:54:58 +0300

    321

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 export const fetchImages = async (search, pagePhoto, limit) => {
   const response = await axios.get(
     `https://pixabay.com/api/?key=44687559-d8ede6072036f50d72c2e92ab`,
@@ -16,7 +17,10 @@ export const fetchImages = async (search, pagePhoto, limit) => {
     }
   );
   if (response.status !== 200) {
-    throw new Error(response.status);
+    throw new iziToast.error({
+      position: 'topRight',
+      message: 'Sorry, there are some errors!',
+    });
   }
   return response.data;
 };
